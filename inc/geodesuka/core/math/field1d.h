21a19dea8adc77493fc3ce39adae4a986942189e
ShaderKitty <ajossart3@gmail.com>
2023-07-07 17:03:31 -0400

    design?

--- inc/geodesuka/core/math/field1d.h
+++ inc/geodesuka/core/math/field1d.h
@@ -15,22 +15,33 @@ namespace geodesuka::core::math {
 	class field1d : public field<int, float, T> {
 	public:
 
-		field1d(int aElementCount, float aLowerBound, float aUpperBound, T aValue);
+		field1d();
+		field1d(float aLowerBound, float aUpperBound, int aElementCount);
+		field1d(float aLowerBound, float aUpperBound, int aElementCount, T aValue);
+
+		// Index Access
+		T operator[](int aIndex) const;
+		T& operator[](int aIndex);
+
+		// Position Access
+		T operator[](float aX) const;
+		T& operator[](float aX);
+
 
 	private:
 
 	};
 
-	template<typename T> inline
-	field1d<T>::field1d(int aElementCount, float aLowerBound, float aUpperBound, T aValue) {
-		ElementCount	= aElementCount;
-		LowerBound		= aLowerBound;
-		UpperBound		= aUpperBound;
-		Data.resize(aElementCount);
-		for (size_t i = 0; i Data.size(); i++) {
-			Data[i] = aValue;
-		}
-	}
+	//template<typename T> inline
+	//field1d<T>::field1d(int aElementCount, float aLowerBound, float aUpperBound, T aValue) {
+	//	ElementCount	= aElementCount;
+	//	LowerBound		= aLowerBound;
+	//	UpperBound		= aUpperBound;
+	//	Data.resize(aElementCount);
+	//	for (size_t i = 0; i Data.size(); i++) {
+	//		Data[i] = aValue;
+	//	}
+	//}
 
 }
 
